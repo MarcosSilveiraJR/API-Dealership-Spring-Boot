45ecf14a342b2f6181f1c39d85c3225416351c8e
MarcosSilveiraJR <marcosrogerio1994@gmail.com>
2022-06-07 15:07:07 -0300

    Inserido anotações Swagger na AddressController

--- src/main/java/com/dealership/apispringbootdealership/controller/address/AddressController.java
+++ src/main/java/com/dealership/apispringbootdealership/controller/address/AddressController.java
@@ -2,6 +2,7 @@ package com.dealership.apispringbootdealership.controller.address;
 
 import com.dealership.apispringbootdealership.controller.address.model.AddressControllerResponse;
 import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
@@ -15,26 +16,31 @@ import java.util.List;
 public class AddressController {
     private AddressFacadeController addressFacade;
 
+    @ApiOperation("Busca um cep no site ViaCep")
     @GetMapping("/{cep}")
     public AddressControllerResponse getCep(@PathVariable String cep) {
         return addressFacade.getCep(cep);
     }
 
+    @ApiOperation("Busca um cep salvo no banco de dados")
     @GetMapping("/id/{cep}")
     public AddressControllerResponse findCep(@PathVariable String cep){
         return addressFacade.findByCep(cep);
     }
 
+    @ApiOperation("Busca todos os cep salvos no banco de dados")
     @GetMapping("/all")
     public List<AddressControllerResponse> findAll(){
         return addressFacade.findAll();
     }
 
-    @GetMapping("/salvar/{cep}")
+    @ApiOperation("Busca um cep no ViaCep e salva no banco de dados")
+    @GetMapping("/save/{cep}")
     public AddressControllerResponse salvaCep(@PathVariable String cep){
         return addressFacade.saveCep(cep);
     }
 
+    @ApiOperation("Deleta todos os ceps salvos no banco de dados")
     @DeleteMapping("/del")
     public void deleteAll(){
         addressFacade.deleteAll();
